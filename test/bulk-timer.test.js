b2bce3e4a2fef7b1aed59ca2fca5a7a68b69bc0d
David Mark Clements <huperekchuno@googlemail.com>
2019-05-26 00:12:15 +0200

    bulk-timer full cov

--- test/bulk-timer.test.js
+++ test/bulk-timer.test.js
@@ -5,7 +5,7 @@ const bulkTimer = require('../lib/bulk-timer')
 
 test('bulk timer queue', async ({ is, same }) => {
   const timer = whenify(bulkTimer)
-  const t = timer(100, function (batch) {
+  const t = timer(100, (batch) => {
     is(batch.length, 2)
     same([ 1, 2 ], batch)
     t.destroy()
@@ -17,7 +17,7 @@ test('bulk timer queue', async ({ is, same }) => {
 
 test('bulk timer queue (async)', async ({ is, same }) => {
   const timer = whenify(bulkTimer)
-  const t = timer(100, function (batch) {
+  const t = timer(100, (batch) => {
     is(batch.length, 2)
     same([ 1, 2 ], batch)
     t.destroy()
@@ -30,7 +30,7 @@ test('bulk timer queue (async)', async ({ is, same }) => {
 
 test('bulk timer queue different batch', async ({ is, same }) => {
   const timer = whenify(bulkTimer, { asyncOps: 2 })
-  const t = timer(100, function (batch) {
+  const t = timer(100, (batch) => {
     if (timer.count === 0) {
       is(batch.length, 1)
       same([ 1 ], batch)
@@ -44,4 +44,19 @@ test('bulk timer queue different batch', async ({ is, same }) => {
   await timeout(75)
   t.push(2)
   await timer.done()
+})
+
+test('bulk timer – nothing pending', async ({ is }) => {
+  var called = false
+  var runs = 0
+  const t = bulkTimer(100, function () {
+    called = !!runs++
+  })
+  t.push(1) // this triggers the interval
+  is(runs, 0)
+  await timeout(200) // wait for pending to clear
+  is(runs, 1)
+  await timeout(200)
+  is(called, false)
+  t.destroy()
 })
