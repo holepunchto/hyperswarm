df08a4d6c2d9d3d24d2418790943e6e29cadfa5d
Andrew Osheroff <andrewosh@gmail.com>
2021-06-16 14:55:21 +0200

    About to refactor PeerQueue

--- example.js
+++ example.js
@@ -3,16 +3,28 @@ const Swarm = require('.')
 start()
 
 async function start () {
-  const swarm = new Swarm({ seed: Buffer.alloc(32).fill(1) })
-  swarm.on('connection', function (connection, info) {
+  const swarm1 = new Swarm({ seed: Buffer.alloc(32).fill(4) })
+  const swarm2 = new Swarm({ seed: Buffer.alloc(32).fill(5) })
+
+  console.log('SWARM 1 KEYPAIR:', swarm1.keyPair)
+  console.log('SWARM 2 KEYPAIR:', swarm2.keyPair)
+
+  swarm1.on('connection', function (connection, info) {
+    console.log('swarm 1 got a server connection:', connection.remotePublicKey, connection.publicKey, info)
+    connection.on('error', err => console.error('1 CONN ERR:', err))
     // Do something with `connection`
     // `info` is a PeerInfo object
   })
+  swarm2.on('connection', function (connection, info) {
+    console.log('swarm 2 got a client connection:', connection.remotePublicKey, connection.publicKey, info)
+    connection.on('error', err => console.error('2 CONN ERR:', err))
+  })
 
-  const key = Buffer.alloc(32).fill(2)
-  const discovery = swarm.join(key)
-  // discovery.refresh({ server: true })
-  await discovery.flushed() // Wait for the first lookup/annnounce to complete.
+  const key = Buffer.alloc(32).fill(7)
+
+  const discovery1 = swarm1.join(key, { server: true, client: false })
+  await discovery1.flushed() // Wait for the first lookup/annnounce to complete.
+
+  const discovery2 = swarm2.join(key, { server: false, client: true })
   // await discovery.destroy() // Stop lookup up and announcing this topic.
-  // console.log('DESTROY FINISHED')
 }
