daf8189eb997f9619ba2166846b2b13224bee50e
HDegroote <75906619+HDegroote@users.noreply.github.com>
2023-10-30 17:36:47 +0100

    (temp) run flaky test many times to see if it's fixed

--- test/retry-timer.js
+++ test/retry-timer.js
@@ -16,28 +16,32 @@ const MAX_JITTER = 20
 const isLinux = process.platform === 'linux'
 
 // Windows and Mac CI are slow, running on Linux only is enough
-test('retry timer - proven peer reinsertion', { skip: !isLinux }, async (t) => {
-  let calls = 0
-  const rt = new RetryTimer(() => calls++, {
-    backoffs: BACKOFFS,
-    jitter: MAX_JITTER
+for (let i = 0; i < 200; i++) {
+  test('retry timer - proven peer reinsertion', { skip: !isLinux }, async (t) => {
+    let calls = 0
+    const rt = new RetryTimer(() => calls++, {
+      backoffs: BACKOFFS,
+      jitter: MAX_JITTER
+    })
+
+    const peerInfo = randomPeerInfo()
+
+    rt.add(peerInfo)
+
+    const msMargin = 50
+    await timeout(BACKOFFS[0] + MAX_JITTER + msMargin)
+    t.is(calls, 1)
+
+    setQuickRetry(peerInfo)
+    rt.add(peerInfo)
+
+    await timeout(BACKOFFS[0] + MAX_JITTER + msMargin)
+
+    t.is(calls, 2)
+
+    rt.destroy()
   })
-
-  const peerInfo = randomPeerInfo()
-
-  rt.add(peerInfo)
-
-  await timeout(BACKOFFS[0] + MAX_JITTER)
-
-  setQuickRetry(peerInfo)
-  rt.add(peerInfo)
-
-  await timeout(BACKOFFS[0] + MAX_JITTER)
-
-  t.is(calls, 2)
-
-  rt.destroy()
-})
+}
 
 test('retry timer - forget unresponsive', async (t) => {
   let calls = 0
