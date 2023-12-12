6aec42aeb8f534250c6dad9d9f6944918226777c
HDegroote <75906619+HDegroote@users.noreply.github.com>
2023-12-12 11:56:37 +0100

    Unflake + clean up single-reconnect test

--- test/swarm.js
+++ test/swarm.js
@@ -79,39 +79,46 @@ test('one server, one client - single reconnect', async (t) => {
   const swarm1 = new Hyperswarm({ bootstrap, backoffs: BACKOFFS, jitter: 0 })
   const swarm2 = new Hyperswarm({ bootstrap, backoffs: BACKOFFS, jitter: 0 })
 
-  const reconnection1Test = t.test('reconnection1')
-  const reconnection2Test = t.test('reconnection2')
+  const serverReconnectsTest = t.test('server reconnects')
+  const clientReconnectsTest = t.test('client reconnects')
 
-  reconnection1Test.plan(1)
-  reconnection2Test.plan(1)
+  serverReconnectsTest.plan(1)
+  clientReconnectsTest.plan(1)
 
   t.teardown(async () => {
     await swarm1.destroy()
     await swarm2.destroy()
   })
 
-  let clientDisconnected = false
+  let hasClientConnected = false
   let serverDisconnected = false
 
   swarm2.on('connection', (conn) => {
     conn.on('error', noop)
-    if (!clientDisconnected) {
-      clientDisconnected = true
-      conn.destroy()
+
+    if (!hasClientConnected) {
+      hasClientConnected = true
       return
     }
-    reconnection2Test.pass('client')
-    conn.end()
+
+    clientReconnectsTest.pass('client reconnected')
   })
-  swarm1.on('connection', (conn) => {
+
+  swarm1.on('connection', async (conn) => {
     conn.on('error', noop)
+
     if (!serverDisconnected) {
       serverDisconnected = true
+
+      // Ensure connection is setup for client too
+      // before we destroy it
+      await flushConnections(swarm2)
+      if (!hasClientConnected) t.fail('Logical error in the test: the client should be connected by now')
+
       conn.destroy()
       return
     }
-    reconnection1Test.pass('client')
-    conn.end()
+    serverReconnectsTest.pass('Server reconnected')
   })
 
   const topic = Buffer.alloc(32).fill('hello world')
