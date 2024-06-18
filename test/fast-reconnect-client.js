1f26e5836ffd488315335efd32727a4ec0e194b2
Billie Hilton <587740+billiegoose@users.noreply.github.com>
2024-06-18 13:05:24 -0400

    WIP

--- test/fast-reconnect-client.js
+++ test/fast-reconnect-client.js
@@ -1,15 +1,17 @@
 const test = require('brittle')
 
 const Hyperswarm = require('..')
+const HyperDHT = require('hyperdht')
 
 test.solo('one server, one client - single reconnect', async (t) => {
   // const { bootstrap } = await createTestnet(3, t.teardown)
   const bootstrap = undefined // ['192.168.1.193:49738']
+  const swarm1 = new Hyperswarm({ bootstrap })
+  console.log('my key:', swarm1.keyPair.publicKey.toString('hex'))
 
   const seed = Buffer.alloc(32).fill('billie-fast-reconnect')
-
-  const swarm1 = new Hyperswarm({ seed, bootstrap })
-  console.log('my key:', swarm1.keyPair.publicKey.toString('hex'))
+  const serverKey = HyperDHT.keyPair(seed)
+  console.log('server key:', serverKey.publicKey)
 
   const reconnectsTest = t.test('reconnects')
 
@@ -19,18 +21,24 @@ test.solo('one server, one client - single reconnect', async (t) => {
     await swarm1.destroy()
   })
 
+  let disconnected = false
+
   swarm1.on('connection', async (conn) => {
     console.log('conn.publicKey', conn.publicKey.toString('hex'))
     console.log('conn.remotePublicKey', conn.remotePublicKey.toString('hex'))
-    conn.on('error', console.log.bind(console))
-    conn.on('close', console.log.bind(console))
-    reconnectsTest.pass('agent connected')
+    conn.on('error', noop)
     console.log('conn.rawStream.id', conn.rawStream.id, 'conn.rawStream.remoteId', conn.rawStream.remoteId)
+    if (!disconnected) {
+      disconnected = true
+
+      reconnectsTest.pass('client terminates initial connection')
+      conn.destroy()
+      return
+    }
+    reconnectsTest.pass('agent reconnected')
   })
 
-  const topic = Buffer.alloc(32).fill('billie-fast-reconnect')
-  swarm1.listen()
-  await swarm1.join(topic, { client: false, server: true }).flushed()
+  swarm1.joinPeer(serverKey.publicKey)
 })
 
 function noop () {}
